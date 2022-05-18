73446a8441215520f7950d2f32d0ec94accf55cd
=abhishektanwar <abhishektanwar084@gmail.com>
2022-05-18 07:07:04 +0530

    code refactor

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,9 @@
 import "./login.css";
-import dummy from '../../components/Header/dummy_profile_img.png'
 import { SignIn } from "../../components";
-// import {Login} from '../../components'
 const Login = () => {
   return (
     <div className="login-container">
       <div className="login-wrapper">
-        {/* <Login /> */}
         <SignIn />
       </div>
     </div>
