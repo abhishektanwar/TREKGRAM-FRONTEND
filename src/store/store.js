6bc9025517eddd81e051230a3a0476483d316066
=abhishektanwar <abhishektanwar084@gmail.com>
2022-05-20 01:18:23 +0530

    fix merge conflix

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
-<<<<<<< HEAD
-=======
 import postReducer from '../reducers/postSlice'
->>>>>>> home-page-functionalities
 import userReducer from '../reducers/userSlice'
 
 
 export const store = configureStore({
   reducer :{
-<<<<<<< HEAD
-=======
     counter:postReducer,
->>>>>>> home-page-functionalities
     user:userReducer
   }
 })
